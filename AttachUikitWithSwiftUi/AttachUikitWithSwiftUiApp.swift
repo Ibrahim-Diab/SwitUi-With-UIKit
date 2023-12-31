7b55f356646c15948e268b1182c19a7d69f31e58
Ibrahim diab <ibrahimdiab011@gmail.com>
2023-12-31 23:57:50 +0200

    Initial Commit

---
+++ AttachUikitWithSwiftUi/AttachUikitWithSwiftUiApp.swift
@@ -0,0 +1,17 @@
+//
+//  AttachUikitWithSwiftUiApp.swift
+//  AttachUikitWithSwiftUi
+//
+//  Created by Diab on 31/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct AttachUikitWithSwiftUiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
